a45c83528587a5c7f519d8832b5470baf0e6954e
ericpkatz <ericpkatz@gmail.com>
2023-08-18 12:44:07 -0400

    finished guided practice

---
+++ script.js
@@ -0,0 +1,27 @@
+function sumValues(obj){
+  let sum = 0;
+  for(let key in obj){
+    sum = sum + obj[key];
+  }
+  return sum;
+}
+
+const dinner = {
+  cheeseburger: 12,
+  steak: 20,
+  soup: 8,
+  macAndCheeze: 14,
+  soupAndSalad: 16
+};
+
+let sum = 0;
+for(let key in dinner){
+  //console.log(key);
+  //console.log(dinner[key]);
+  sum = sum + dinner[key];
+}
+console.log(sumValues(dinner));
+console.log(sumValues({}));
+console.log(sumValues({foo: 'bar'}));
+//console.log(Object.keys(dinner));
+//console.log(Object.values(dinner));
